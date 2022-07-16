c354e8ecdd433ab84c3ce84e8050face25661dc4
Pedro Piñera Buendía <pedro@craftweg.com>
2022-07-16 13:13:16 +0200

    Add authentication commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser,Subcommand};
 use clap::crate_version;
 
 #[derive(Parser, Debug)]
@@ -7,19 +7,35 @@ use clap::crate_version;
 #[clap(about = "A CLI tool to deploy projects through a Catalysis instance", long_about = None)]
 #[clap(propagate_version = true)]
 struct Cli {
-    /// Name of the person to greet
-    #[clap(short, long, value_parser)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, value_parser, default_value_t = 1)]
-    count: u8,
+    #[clap(subcommand)]
+   commands: Commands
 }
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[clap(about = "Manage authentication sessions")]
+    Auth {
+        #[clap(subcommand)]
+        commands: AuthCommands
+    },
+
+    #[clap(about = "Run a new deployment")]
+    Deploy {}
+}
+
+#[derive(Subcommand, Debug)]
+enum AuthCommands {
+    #[clap(about = "Authenticate with an instance of Catalysis")]
+    Login {
+
+    },
+    #[clap(about = "Revoke an existing session and delete it locally")]
+    Logout {
+
+    }
+}
 
 pub fn run() {
     let args = Cli::parse();
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+
 }
